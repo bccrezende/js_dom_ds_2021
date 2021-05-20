0d7eb92250d7f12e8540922b443422eea1308e4a
Barbara Rezende <bccrezende@gmail.com>
2021-05-19 22:09:56 -0300

    add/remove tasks

--- js/listaDeTarefas.js
+++ js/listaDeTarefas.js
@@ -1,16 +1,43 @@
 const novaTarefa = document.querySelector('[data-form-button]')
-const inputTarefa = document.querySelector('[data-form-input]')
+
+
 
 function criarTarefa(evento){
     evento.preventDefault()
     //console.log(inputTarefa.value)
-    const tarefa = document.querySelector('[data-task]')
-    const conteudo = `<label class="form-check-label">${inputTarefa.value}</label>`
+    const lista = document.querySelector('[data-list]')
+    const inputTarefa = document.querySelector('[data-form-input]')
+    const valor = inputTarefa.value
+    
+    const tarefa = document.createElement('li')
+    const conteudo = `<label class="form-check-label"> ${valor}</label>`
+    
     tarefa.innerHTML = conteudo
 
+    tarefa.appendChild(botaoConclui())
+    lista.appendChild(tarefa)
+
     inputTarefa.value = " "
+}
+
+function botaoConclui(){
+    const botaoConclui = document.createElement('button')
+    botaoConclui.classList = 'remove mdi mdi-close-circle-outline'
+
+    botaoConclui.addEventListener('click', concluirEvento)
+
+    return botaoConclui;
+}
+
+function concluirEvento(evento){
+    const botaoConclui = evento.target
+    const tarefaCompleta = botaoConclui.parentElement
+    console.log(tarefaCompleta)
+    tarefaCompleta.classList.toggle('done')
 
 }
 
+
 novaTarefa.addEventListener('click', criarTarefa)
+
 

37ab1c1537f4f5783c188b0d3b609e342cae954a
Barbara Rezende <bccrezende@gmail.com>
2021-06-13 21:30:45 -0300

    remover tarefa

--- js/listaDeTarefas.js
+++ js/listaDeTarefas.js
@@ -2,6 +2,16 @@
     const novaTarefa = document.querySelector('[data-form-button]')
     const inputTarefa = document.querySelector('[data-form-input]')
 
+    function criarBotaoDelete(){
+        const botaoDelete = document.createElement('span')
+        botaoDelete.classList = "close"
+        botaoDelete.innerText = "x"
+
+        botaoDelete.addEventListener('click', deletarTarefa)
+
+        return botaoDelete;
+    }
+
     function criarTarefa(evento){
         evento.preventDefault()
 
@@ -14,6 +24,7 @@
 
         novoItem = document.createElement('li')
         novoItem.appendChild(novaLabel)
+        novoItem.appendChild(criarBotaoDelete())
 
         listaDeTarefas.appendChild(novoItem)
 
@@ -21,6 +32,14 @@
 
     }
 
+    function deletarTarefa(evento){
+        const BotaoDeletarClicado = evento.target
+        const TarefaDeletar = BotaoDeletarClicado.parentElement
+        console.log(TarefaDeletar)
+        TarefaDeletar.remove()
+        
+        console.log('vamos deletar a tarefa?')
+    }
 
     novaTarefa.addEventListener('click', criarTarefa)
 })()

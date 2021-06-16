30ea90885169a0f8e521b64f2b3c076770c12695
Barbara Rezende <bccrezende@gmail.com>
2021-06-16 09:42:45 -0300

    botao remover tarefa

--- js/listaDeTarefas.js
+++ js/listaDeTarefas.js
@@ -2,6 +2,23 @@
     const novaTarefa = document.querySelector('[data-form-button]')
     const inputTarefa = document.querySelector('[data-form-input]')
 
+    function criarBotaoDelete(){
+        const botaoDelete = document.createElement('span')
+        botaoDelete.innerText = "x"
+        botaoDelete.classList = "close"
+
+        botaoDelete.addEventListener('click', deletarTarefa)
+        
+        console.log(botaoDelete)
+
+        return botaoDelete
+    }
+
+    function deletarTarefa(){
+        
+        console.log('Por favor, delete essa tarefa')
+    }
+
     function criarTarefa(evento){
         evento.preventDefault()
 
@@ -9,12 +26,13 @@
         const listaDeTarefas = document.querySelector('[data-task]')
 
         novaLabel = document.createElement('label')
-        novaLabel.innerText = valorTarefa
+        novaLabel.innerText = `- ${valorTarefa}`;
         novaLabel.className = "form-check-label"
 
         novoItem = document.createElement('li')
         novoItem.appendChild(novaLabel)
-
+        novoItem.appendChild(criarBotaoDelete())
+        
         listaDeTarefas.appendChild(novoItem)
 
         inputTarefa.value = ""
